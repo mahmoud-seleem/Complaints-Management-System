c31439dd33ce859a30a446cdf20f85e5f7e8964f
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-03-05 12:57:06 +0200

    initial commit

---
+++ src/main/java/com/example/Complaints/Management/System/ComplaintsManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Complaints.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Complaints/Management/System/ComplaintsManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Complaints.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintsManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
