b7a832ce42e4e48c982373770bfcb16a3fe98fbc
mahmoud-seleem <mahmoudsaleem522@gmail.com>
2025-03-10 16:44:15 +0200

    validation of user registration

--- src/main/java/com/example/Complaints/Management/System/shared/Utils/Validation.java
+++ src/main/java/com/example/Complaints/Management/System/shared/Utils/Validation.java
@@ -3,7 +3,9 @@ package com.example.Complaints.Management.System.shared.Utils;
 import com.example.Complaints.Management.System.core.application.dto.AdminDto;
 import com.example.Complaints.Management.System.core.application.dto.GeneralUserDto;
 import com.example.Complaints.Management.System.core.domain.entities.Admin;
+import com.example.Complaints.Management.System.core.domain.entities.GeneralUser;
 import com.example.Complaints.Management.System.core.infrastructure.Repository.AdminRepo;
+import com.example.Complaints.Management.System.core.infrastructure.Repository.GeneralUserRepo;
 import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,10 +17,13 @@ public class Validation {
     private AdminRepo adminRepo;
 
     public void validateAdminRegistrationData(AdminDto adminDto) throws NoSuchFieldException, IllegalAccessException {
+        ensureGeneralUserDoesNotExist(adminDto);
         ensureFieldIsNull(AdminDto.class,adminDto,"userId");
+        ensureFieldIsNull(AdminDto.class,adminDto,"token");
         validateGeneralUserRequiredFieldsAreNotNull(adminDto);
-        }
-
+        ensureUserNameAndPasswordAreValid(adminDto);
+        
+    }
 
         public void validateGeneralUserRequiredFieldsAreNotNull(GeneralUserDto dto){
             isNotNull(dto.getUserName(),"userName");
@@ -39,6 +44,33 @@ public class Validation {
             }
     }
 
+    public void ensureUserNameAndPasswordAreValid(GeneralUserDto dto){
+        if (dto.getUserName().length() > 0 ){
+            if (dto.getPassword().length() >= 5){
+            }else {
+                throw new CustomValidationException(
+                        "password should be more than 5 chars !!",
+                        "password",
+                        dto.getPassword());
+            }
+        }else {
+            throw new CustomValidationException(
+                    "userName should not be empty !",
+                    "userName",
+                    dto.getUserName());
+        }
+        }
+
+
+    public void ensureGeneralUserDoesNotExist(GeneralUserDto dto){
+        GeneralUser user = adminRepo.findByUserName(dto.getUserName());
+        if (user != null){
+            throw new CustomValidationException(
+                    "User with this userName Already Exists !",
+                    "userName",
+                    dto.getUserName());
+        }
+    }
     public Admin isAdminExist(Long id){
         try {
             return adminRepo.findById(id).get();
